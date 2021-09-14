740da7372bdffeba517b80b4fff2506ae7f3af7b
Nguyen Ngoc Linh <mail2ngoclinh@gmail.com>
2021-09-15 00:56:00 +0700

    setup unit tests

---
+++ test/words/tests.py
@@ -0,0 +1,74 @@
+import sys
+from pathlib import Path
+from testhelper import nasm, test_case
+from testhelper import expect_status_to_be, expect_stdout_to_be
+from termcolor import colored
+from functools import reduce
+import random
+
+def build_utils():
+    target = 'utils'
+    source = '../../src/utils.asm'
+    (ok, msg) = nasm(target, [source])
+    if not ok:
+        print(colored(msg, 'red'))
+
+def build_next():
+    target = 'next'
+    source = '../../src/next.asm'
+    (ok, msg) = nasm(target, [source])
+    if not ok:
+        print(colored(msg, 'red'))
+
+def setup():
+    cwd = Path.cwd()
+
+    # A directory to store all test object files.
+    (cwd / 'obj').mkdir(exist_ok=True)
+
+    # And a directory to store all test binary files.
+    (cwd / 'bin').mkdir(exist_ok=True)
+
+    # Build utils object file
+    build_utils()
+
+    # Build next object file
+    build_next()
+
+#
+# TEST SUITES.
+#
+
+TEST_SUITES = {
+}
+
+def print_summary(summary):
+    (total_count, failure_count) = summary
+
+    s1 = '{} tests, '.format(total_count)
+    s2 = '{} failures'.format(failure_count)
+
+    print('-' * len(s1 + s2))
+
+    s1 = colored(s1, 'green')
+    s2 = colored(s2, 'red' if failure_count else 'green')
+    print(s1 + s2)
+
+if __name__ == '__main__':
+    setup()
+
+    suites = []
+    argv = sys.argv
+
+    if len(argv) > 1:
+        suites = [TEST_SUITES.get(arg) for arg in argv]
+        suites = filter(lambda x : x is not None, suites)
+    else:
+        suites = TEST_SUITES.values()
+
+    suites = list(suites)
+    random.shuffle(suites)
+
+    summary = [t() for t in suites]
+    summary = reduce(lambda a,x : (a[0]+x[0], a[1]+x[1]), summary, (0, 0))
+    print_summary(summary)
